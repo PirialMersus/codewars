62456b2d1218eb893a014946eb06cdf3034db27e
PirialMersus <44650069+PirialMersus@users.noreply.github.com>
2021-04-24 20:56:49 +0300

    alone work 1

--- js/main1.js
+++ js/main1.js
@@ -660,17 +660,17 @@
 //     n * c
 //   } dollars to buy liquid`;
 // }
-function evenLast(numbers) {
-  if (numbers.length === 0) return 0;
-  let sum = 0;
-  for (let i = 0; i < numbers.length; i++) {
-    if (i % 2 === 0) {
-      sum = sum + numbers[i];
-    }
-    if (i === numbers.length - 1) {
-      sum = sum * numbers[i];
-    }
-  }
-  return sum;
-}
-console.log(evenLast([2, 3, 4, 5]));
+// function evenLast(numbers) {
+//   if (numbers.length === 0) return 0;
+//   let sum = 0;
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (i % 2 === 0) {
+//       sum = sum + numbers[i];
+//     }
+//     if (i === numbers.length - 1) {
+//       sum = sum * numbers[i];
+//     }
+//   }
+//   return sum;
+// }
+// console.log(evenLast([2, 3, 4, 5]));
